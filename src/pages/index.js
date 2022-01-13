d7689da0c7690618bfc5962b889aaf9e6b3f86f5
tomdaniels <thomsondaniels@hotmail.com>
2022-01-13 17:11:29 +1100

    init dijkstras

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import generateGrid from "../utils/generate-grid";
+import { dijkstra } from "../algo/dijkstra";
 import Node from "../components/node";
 
 import styles from "../styles/Grid.module.css";
@@ -32,25 +33,37 @@ export default function PathFinder() {
     setGrid(cells);
   }, []);
 
+  function visualiseDijkstra() {
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    console.log(visitedNodesInOrder);
+  }
+
   return (
-    <div className={styles.grid}>
-      {grid.map((row, rowIdx) => (
-        <div key={rowIdx}>
-          {row.map((node, nodeIdx) => {
-            const { isStart, isFinish } = node;
-            return (
-              <React.Fragment key={nodeIdx}>
-                <Node
-                  // row={rowIdx}
-                  // col={nodeIdx}
-                  isStart={isStart}
-                  isFinish={isFinish}
-                />
-              </React.Fragment>
-            );
-          })}
-        </div>
-      ))}
-    </div>
+    <>
+      <button onClick={() => visualiseDijkstra()}>
+        {"Visualise Dijkstra's Algorithm"}
+      </button>
+      <div className={styles.grid}>
+        {grid.map((row, rowIdx) => (
+          <div key={rowIdx}>
+            {row.map((node, nodeIdx) => {
+              const { isStart, isFinish } = node;
+              return (
+                <React.Fragment key={nodeIdx}>
+                  <Node
+                    // row={rowIdx}
+                    // col={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  />
+                </React.Fragment>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
