90d7b7306930a0cb4694d7af1db69b97ce07d7a6
tomdaniels <thomsondaniels@hotmail.com>
2022-01-14 14:42:37 +1100

    init maze

---
+++ src/algo/maze/recursive-divison.js
@@ -0,0 +1,198 @@
+import nodeStyles from "../../styles/Node.module.css";
+
+function recursiveDivisionMaze(
+  grid,
+  rowStart,
+  rowEnd,
+  colStart,
+  colEnd,
+  orientation = "",
+  surroundingWalls,
+  type
+) {
+  if (rowEnd < rowStart || colEnd < colStart) {
+    return;
+  }
+  if (!surroundingWalls) {
+    grid.forEach((row) => {
+      row.forEach((cell, idx) => {
+        const { row: r, col: c } = cell;
+        if (
+          r === 0 ||
+          c === 0 ||
+          r === grid.length - 1 ||
+          c === row.length - 1
+        ) {
+          setTimeout(() => {
+            let currentHTMLNode = document.getElementById(`node-${r}-${c}`);
+
+            currentHTMLNode.className = `${nodeStyles.node} ${nodeStyles.nodeWall}`;
+            cell.isWall = true;
+          }, 20 * idx);
+        }
+      });
+    });
+    surroundingWalls = true;
+  }
+  if (orientation === "horizontal") {
+    let possibleRows = [];
+    for (let number = rowStart; number <= rowEnd; number += 2) {
+      possibleRows.push(number);
+    }
+    let possibleCols = [];
+    for (let number = colStart - 1; number <= colEnd + 1; number += 2) {
+      possibleCols.push(number);
+    }
+    let randomRowIndex = Math.floor(Math.random() * possibleRows.length);
+    let randomColIndex = Math.floor(Math.random() * possibleCols.length);
+    let currentRow = possibleRows[randomRowIndex];
+    let colRandom = possibleCols[randomColIndex];
+    grid.forEach((row) => {
+      row.forEach((cell, idx) => {
+        const { row: r, col: c } = cell;
+
+        if (
+          r === currentRow &&
+          c !== colRandom &&
+          c >= colStart - 1 &&
+          c <= colEnd + 1
+        ) {
+          setTimeout(() => {
+            if (!cell.isStart && !cell.isFinish) {
+              let currentHTMLNode = document.getElementById(`node-${r}-${c}`);
+              currentHTMLNode.className = `${nodeStyles.node} ${nodeStyles.nodeWall}`;
+              cell.isWall = true;
+            }
+          }, 30 * idx);
+        }
+      });
+    });
+    if (currentRow - 2 - rowStart > colEnd - colStart) {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        currentRow - 2,
+        colStart,
+        colEnd,
+        orientation,
+        surroundingWalls,
+        type
+      );
+    } else {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        currentRow - 2,
+        colStart,
+        colEnd,
+        "vertical",
+        surroundingWalls,
+        type
+      );
+    }
+    if (rowEnd - (currentRow + 2) > colEnd - colStart) {
+      recursiveDivisionMaze(
+        grid,
+        currentRow + 2,
+        rowEnd,
+        colStart,
+        colEnd,
+        orientation,
+        surroundingWalls,
+        type
+      );
+    } else {
+      recursiveDivisionMaze(
+        grid,
+        currentRow + 2,
+        rowEnd,
+        colStart,
+        colEnd,
+        "vertical",
+        surroundingWalls,
+        type
+      );
+    }
+  } else {
+    let possibleCols = [];
+    for (let number = colStart; number <= colEnd; number += 2) {
+      possibleCols.push(number);
+    }
+    let possibleRows = [];
+    for (let number = rowStart - 1; number <= rowEnd + 1; number += 2) {
+      possibleRows.push(number);
+    }
+    let randomColIndex = Math.floor(Math.random() * possibleCols.length);
+    let randomRowIndex = Math.floor(Math.random() * possibleRows.length);
+    let currentCol = possibleCols[randomColIndex];
+    let rowRandom = possibleRows[randomRowIndex];
+    grid.forEach((row) => {
+      row.forEach((cell, idx) => {
+        const { row: r, col: c } = cell;
+        if (
+          c === currentCol &&
+          r !== rowRandom &&
+          r >= rowStart - 1 &&
+          r <= rowEnd + 1
+        ) {
+          let currentHTMLNode = document.getElementById(`node-${r}-${c}`);
+          if (!cell.isStart && !cell.isFinish) {
+            setTimeout(() => {
+              currentHTMLNode.className = `${nodeStyles.node} ${nodeStyles.nodeWall}`;
+              cell.isWall = true;
+            }, 50 * idx);
+          }
+        }
+      });
+    });
+
+    if (rowEnd - rowStart > currentCol - 2 - colStart) {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        rowEnd,
+        colStart,
+        currentCol - 2,
+        "horizontal",
+        surroundingWalls,
+        type
+      );
+    } else {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        rowEnd,
+        colStart,
+        currentCol - 2,
+        orientation,
+        surroundingWalls,
+        type
+      );
+    }
+    if (rowEnd - rowStart > colEnd - (currentCol + 2)) {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        rowEnd,
+        currentCol + 2,
+        colEnd,
+        "horizontal",
+        surroundingWalls,
+        type
+      );
+    } else {
+      recursiveDivisionMaze(
+        grid,
+        rowStart,
+        rowEnd,
+        currentCol + 2,
+        colEnd,
+        orientation,
+        surroundingWalls,
+        type
+      );
+    }
+  }
+}
+
+export default recursiveDivisionMaze;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { toggleWalls, generateGrid } from "../utils";
 import { dijkstra, getNodesInShortestPathOrder } from "../algo/dijkstra";
+import recursiveDivisionMaze from "../algo/maze/recursive-divison";
 import Node from "../components/node";
 
 import styles from "../styles/Grid.module.css";
@@ -13,6 +14,7 @@ const FINISH_NODE_ROW = 10;
 
 export default function PathFinder() {
   const [grid, setGrid] = useState([]);
+  const [gridColLength, setGridLength] = useState(0);
   const [finishNodeCol, setFinishNodeCol] = useState(0);
   const [pressedMouse, setPressedMouse] = useState(false);
 
@@ -33,11 +35,11 @@ export default function PathFinder() {
 
   useEffect(() => {
     // build grid & set finish node using current browser width
-    const GRID_COL_LENGTH = window.innerWidth / 22;
+    const GRID_COL_LENGTH = Math.floor(window.innerWidth / 22);
     const FINISH_NODE_COL = Math.floor(GRID_COL_LENGTH - 10);
 
-    // so our click handlers can access width based column..
-    setFinishNodeCol(FINISH_NODE_COL);
+    setGridLength(GRID_COL_LENGTH); // accessed from maze gen
+    setFinishNodeCol(FINISH_NODE_COL); // accessed from click handlers
 
     const cells = generateGrid(
       GRID_ROW_LENGTH,
@@ -87,6 +89,18 @@ export default function PathFinder() {
     animate(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
+  function generateMaze() {
+    const maze = recursiveDivisionMaze(
+      grid,
+      2,
+      GRID_ROW_LENGTH,
+      2,
+      gridColLength,
+      "horizontal"
+    );
+    console.log(maze);
+  }
+
   function handleMouseDown(row, col) {
     const _grid = toggleWalls(grid, row, col);
     setGrid(_grid);
@@ -108,6 +122,7 @@ export default function PathFinder() {
       <button onClick={() => visualiseDijkstra()}>
         {"Visualise Dijkstra's Algorithm"}
       </button>
+      <button onClick={() => generateMaze()}>Generate Maze</button>
       <div className={styles.grid}>
         {grid.map((row, rowIdx) => (
           <div key={rowIdx} className={styles.row}>
