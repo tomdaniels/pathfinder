82414633c149830c29ce49a10dcf685b6016a466
tomdaniels <thomsondaniels@hotmail.com>
2023-03-08 09:50:44 +1100

    refactor button group

--- src/components/visualiser/button-group.js
+++ src/components/visualiser/button-group.js
@@ -1,15 +1,4 @@
-import {
-  SegmentedControl,
-  SegmentedControlOption,
-} from "@leafygreen-ui/segmented-control";
-
 import styles from "../../styles/ButtonGroup.module.css";
-
-const TASK_TYPES = {
-  GEN_MAZE: "generate-maze",
-  VISUALISE: "visualise",
-  CLEAR: "clear",
-};
 
 export default function ButtonGroup({
   generateMaze,
@@ -17,42 +6,19 @@ export default function ButtonGroup({
   locked,
   clear,
 }) {
-  const TASKS = {
-    [TASK_TYPES.VISUALISE]: () => visualiseAlgo(),
-    [TASK_TYPES.GEN_MAZE]: () => generateMaze(),
-    [TASK_TYPES.CLEAR]: () => clear(),
-  };
-
-  function handleTask(type) {
-    TASKS[type]();
-  }
   return (
     <div className={styles.container}>
-      <SegmentedControl
-        label="path visualiser"
-        size={"default"}
-        darkMode={false}
-        followFocus={true}
-        aria-controls="grid-control"
-        defaultValue={TASK_TYPES.CLEAR}
-        onChange={(type) => {
-          handleTask(type);
-        }}
-      >
-        <SegmentedControlOption
-          disabled={locked}
-          onClick={() => clear()}
-          value={TASK_TYPES.CLEAR}
-        >
+      <div className={styles.left}>
+        <button disabled={locked} onClick={() => generateMaze()}>
+          Generate maze
+        </button>
+        <button disabled={locked} onClick={() => clear()}>
           Clear
-        </SegmentedControlOption>
-        <SegmentedControlOption disabled={locked} value={TASK_TYPES.VISUALISE}>
-          Visualise
-        </SegmentedControlOption>
-        <SegmentedControlOption disabled={locked} value={TASK_TYPES.GEN_MAZE}>
-          Generate Maze
-        </SegmentedControlOption>
-      </SegmentedControl>
+        </button>
+      </div>
+      <button disabled={locked} onClick={() => visualiseAlgo()}>
+        Visualise
+      </button>
     </div>
   );
 }
