563da1ab9ab6c13e1e7ea869adc795c2dcd86b65
Tommy Daniels <thomsondaniels@hotmail.com>
2023-08-13 15:08:39 +1000

    solving algo info

--- src/components/control-group.js
+++ src/components/control-group.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "../styles/ButtonGroup.module.css";
 
 export default function ControlGroup({
@@ -10,6 +11,8 @@ export default function ControlGroup({
 
   children,
 }) {
+  const [showInfo, setShowInfo] = useState("");
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -24,7 +27,10 @@ export default function ControlGroup({
           >
             Generate a maze
           </button>
-          <button disabled={locked} onClick={() => clear()}>
+          <button
+            disabled={!activeMaze || locked || isSolved}
+            onClick={() => clear()}
+          >
             Clear
           </button>
         </div>
@@ -33,18 +39,53 @@ export default function ControlGroup({
       {children}
 
       <div className={styles["solver-controls"]}>
-        <button
-          disabled={locked || isSolved}
-          onClick={() => visualiseAlgo("dijkstra")}
+        <div
+          onMouseEnter={() => setShowInfo("dijkstra")}
+          onMouseLeave={() => setShowInfo("")}
         >
-          Solve using Dijkstra
-        </button>
-        <button
-          disabled={locked || isSolved}
-          onClick={() => visualiseAlgo("backtracker")}
+          <button
+            disabled={locked || isSolved}
+            onClick={() => visualiseAlgo("dijkstra")}
+          >
+            Solve using Dijkstra
+          </button>
+          {showInfo === "dijkstra" && (
+            <>
+              <p>The shortest path is guaranteed with this solution.</p>
+              <p>
+                Dijkstra&apos;s algorithm will search every available node in
+                each possible direction until it reaches the target node.
+              </p>
+            </>
+          )}
+        </div>
+        <div
+          onMouseEnter={() => setShowInfo("dijkstra")}
+          onMouseLeave={() => setShowInfo("")}
         >
-          Solve using recursive backtracking
-        </button>
+          <button
+            disabled={locked || isSolved}
+            onMouseEnter={() => setShowInfo("backtracker")}
+            onClick={() => visualiseAlgo("backtracker")}
+          >
+            Solve using recursive backtracking
+          </button>
+          {showInfo === "backtracker" && (
+            <>
+              <p>The shortest path is not guaranteed with this solution.</p>
+              <p>
+                Recursive backtracking is a Depth First Search algorithm, which
+                randomly selects a path to exhaust until it traps it self. At
+                which point, it &quot;backtracks&quot; to the last branch with
+                another possible solution and repeats until the maze is solved.
+              </p>
+              <p>
+                Due to this random nature, the speed can vary drastically and
+                results may be inconsisten
+              </p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
